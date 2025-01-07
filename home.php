ef5d5c312e2e92def71f2324f78754479629333d
Bhawana <bhwbi.rawal@gmail.com>
2025-01-08 00:33:04 +0545

    added navbar and home page

--- home.php
+++ home.php
@@ -5,24 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="home.css">
 </head>
 <body>
     
 <nav class="navbar navbar-expand-lg fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-auto" href="#">Logo</a>
+    <a class="navbar-brand me-auto"   href="#"><p><img src="images/icon-3.jpg" width="49px"><span>Devalaya</span></p></a>
    
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Devalaya</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
           <li class="nav-item">
             <a class="nav-link mx-lg-2 active" aria-current="page" href="#">Home</a>
           </li>
@@ -32,17 +34,68 @@
           <li class="nav-item">
             <a class="nav-link mx-lg-2" href="#">Contact</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="#">Location</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="#">Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" href="#">Temple</a>
+          </li>
         </ul>
       </div>
     </div>
     <a href="#" class="log-in-button">Login</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
   </div>
 </nav>
+<div id="myCarousel" class="carousel slide">
+ 
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+  
+  </ol>
 
+ 
+  <div class="carousel-inner">
+    <div class="item active">
+     
+      <div class="fill">
+        <img src="images/home-3.webp">
+      </div>
+      <div class="carousel-caption">
+        <h1 class="animated fadeInLeft">Bhagavad Gita</h1>
+        <h4 class="animated fadeInUp">“Believe me ! I am the only truth”</h4>
+        <p class="animated fadeInUp"><a href="#" class="btn btn-transparent btn-rounded btn-large">Learn More</a></p>
+      </div>
+    </div>
+    <div class="item">
+      
+      <div class="fill">
+        <img src="images/home-4.webp" alt="">
+      </div>
+      <div class="carousel-caption">
+        <h1 class="animated fadeInDown">Bhagavad Gita</h1>
+        <h4 class="animated fadeInUp">“You are only entitled to the action, never to its fruits.”</h4>
+        <p class="animated fadeInUp"><a href="#" class="btn btn-transparent btn-rounded btn-large">Learn More</a></p>
+      </div>
+    </div>
+  </div>
 
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="icon-prev"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="icon-next"></span>
+  </a>
+
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
