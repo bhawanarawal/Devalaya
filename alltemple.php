926149a8895af67026a9e4a0b21e7730317e538c
Bhawana <bhwbi.rawal@gmail.com>
2025-02-17 22:35:22 +0545

    updated title

--- alltemple.php
+++ alltemple.php
@@ -27,7 +27,7 @@ if (isset($_POST["like"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Temples</title>
     <!-- Font Awesome -->
     <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
